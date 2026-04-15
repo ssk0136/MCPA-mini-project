90e1da7a2669dd48f139ffb832f99b07c2bcb3e4
ssk0136 <shamanthshekar136@gmail.com>
2026-04-16 01:12:11 +0530

    Refactor code to use I2C LCD and improve moisture logic

--- MPCA MP code.c
+++ MPCA MP code.c
@@ -1,56 +1,63 @@
-#include <dht.h>
-#include <LiquidCrystal.h>
-
-LiquidCrystal lcd(7,8,9,10,11,12);
-
-dht DHT;
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <DHT.h>
 
 #define DHT11_PIN 5
+#define DHTTYPE DHT11
+#define SENSOR_PIN A0
+#define RELAY_PIN 6
 
-int sensor_pin = A0;
-int output_value;
-int relayPin = 6;
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
+DHT dht(DHT11_PIN, DHTTYPE);
 
-void setup(){
-  lcd.begin(16, 2);
-  pinMode(sensor_pin, INPUT);
-  pinMode(relayPin, OUTPUT);
+void setup() {
+  dht.begin();
+  lcd.init();
+  lcd.backlight(); 
+  
+  pinMode(SENSOR_PIN, INPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+  
+  digitalWrite(RELAY_PIN, HIGH); 
+
+  lcd.setCursor(0, 0);
+  lcd.print("System Ready");
+  delay(2000);
 }
 
-void loop(){
-  int chk = DHT.read11(DHT11_PIN);
+void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  int moisture_raw = analogRead(SENSOR_PIN);
+  
+  int moisture_percent = map(moisture_raw, 550, 10, 0, 100);
 
-  lcd.setCursor(0,0); 
-  lcd.print("Temp: ");
-  lcd.print(DHT.temperature);
-  lcd.print((char)223);
-  lcd.print("C");
-  
-  lcd.setCursor(0,1); 
-  lcd.print("Humidity: ");
-  lcd.print(DHT.humidity);
-  lcd.print("%");
-  delay(2000);
-  
-  lcd.setCursor(23,0);
-  lcd.autoscroll();
-  output_value = analogRead(sensor_pin);
-  output_value = map(output_value,550,10,0,100);
-  lcd.print("Mositure: ");
-  lcd.print(output_value);
-  lcd.print("%");
-   
-  lcd.setCursor(23,1);
-  if(output_value < 30){
-    digitalWrite(relayPin, LOW);
-    lcd.print("Motor ON");
-  }
-  else {
-    digitalWrite(relayPin, HIGH);
-    lcd.print("Motor OFF");       
-  }
-  
-  delay(2500);
-  lcd.noAutoscroll();
   lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: "); lcd.print(t, 1); lcd.print((char)223); lcd.print("C");
+  lcd.setCursor(0, 1);
+  lcd.print("Humid: "); lcd.print(h, 1); lcd.print("%");
+  delay(2500);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Moisture: ");
+  lcd.print(moisture_percent);
+  lcd.print("%");
+
+  lcd.setCursor(0, 1);
+  
+  if(moisture_percent >= 45) {
+    digitalWrite(RELAY_PIN, HIGH); 
+    lcd.print("Motor: OFF");
+  } 
+  else if(moisture_percent < 30) {
+    digitalWrite(RELAY_PIN, LOW); 
+    lcd.print("Motor: ON");
+  } 
+  else {
+    lcd.print("Motor: Standby");
+  }
+
+  delay(2500);
 }
